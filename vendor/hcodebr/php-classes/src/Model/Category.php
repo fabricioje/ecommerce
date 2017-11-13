207802d6c6fdf4909d73c2f24ac58c3129aa120a
Fabricio J. Silva <fabricio_fx@hotmail.com>
2017-11-13 20:42:25 -0200

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -28,6 +28,8 @@ class Category extends Model{
 		));
 
 		$this->setData($results[0]);
+
+		Category::updateFile();
 	}
 
 	//busca a categoria pelo id do banco
@@ -49,6 +51,23 @@ class Category extends Model{
 		$results = $sql->select("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
 			':idcategory'=>$this->getidcategory()
 		]);
+
+		Category::updateFile();
+	}
+
+	public static function updateFile(){
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+
+
 	}
 
 }
